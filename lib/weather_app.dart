6e4a460a9326f64aae45aae48930b8feb8d85ba2
Santoroclass <mamatkulov_s@bk.ru>
2023-02-10 22:58:48 +0500

    исправление ключа json

--- lib/weather_app.dart
+++ lib/weather_app.dart
@@ -1,8 +1,10 @@
+
+
 import 'package:flutter/material.dart';
+import 'package:flutter_localizations/flutter_localizations.dart';
 import 'package:provider/provider.dart';
 import 'package:weather_app/domain/provider/weather_provider.dart';
 import 'package:weather_app/ui/routes/app_navigator.dart';
-// import 'package:weather_app/ui/routes/app_routes.dart';
 
 
 class WeatherApp extends StatelessWidget {
@@ -11,7 +13,7 @@ class WeatherApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return ChangeNotifierProvider(
-      create: (context) => WeatherProvider(),
+      create: (context)=>WeatherProvider(),
       child: const MainAppWidget());
   }
 }
@@ -23,10 +25,15 @@ class MainAppWidget extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      debugShowCheckedModeBanner: false,
-      initialRoute: AppNavigator.initRoute,
-      routes: AppNavigator.routes,
-      onGenerateRoute: AppNavigator.generate,
+     localizationsDelegates: const [
+      GlobalCupertinoLocalizations.delegate,
+      GlobalWidgetsLocalizations.delegate,
+      GlobalMaterialLocalizations.delegate,
+     ],
+     debugShowCheckedModeBanner: false,
+     initialRoute: AppNavigator.initRoute,
+     routes: AppNavigator.routes,
+     onGenerateRoute: AppNavigator.generate,
     );
   }
 }
