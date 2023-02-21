83b304eeb539f857f1e8f65e7279c8ebcec028bb
Santoroclass <mamatkulov_s@bk.ru>
2023-02-21 23:50:26 +0500

    favorite_list_ui

--- lib/main.dart
+++ lib/main.dart
@@ -1,21 +1,25 @@
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
 import 'package:flutter_dotenv/flutter_dotenv.dart';
+import 'package:hive_flutter/hive_flutter.dart';
+import 'package:weather_app/domain/hive/favourite_history/favorite_history.dart';
+import 'package:weather_app/domain/hive/hive_boxes.dart';
+
 import 'package:weather_app/weather_app.dart';
 
-
-
-
-Future <void> main() async{
+Future<void> main() async {
   SystemChrome.setSystemUIOverlayStyle(
-   const SystemUiOverlayStyle(
+    const SystemUiOverlayStyle(
       statusBarColor: Colors.transparent,
     ),
   );
+
+  await Hive.initFlutter();
+  Hive.registerAdapter(FavouriteHistoryAdapter());
+  await Hive.openBox<FavouriteHistory>(HiveBoxes.favouriteBox);
+
   await dotenv.load(fileName: '.env');
   runApp(
-    const WeatherApp()
+    const WeatherApp(),
   );
 }
-
-
