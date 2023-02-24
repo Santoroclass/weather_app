bc5f20cbbc0186a24bf6c8500858eb8bce267083
Santoroclass <mamatkulov_s@bk.ru>
2023-02-24 21:49:55 +0500

    splash_screen_ui

--- lib/main.dart
+++ lib/main.dart
@@ -4,7 +4,6 @@ import 'package:flutter_dotenv/flutter_dotenv.dart';
 import 'package:hive_flutter/hive_flutter.dart';
 import 'package:weather_app/domain/hive/favourite_history/favourite_history.dart';
 import 'package:weather_app/domain/hive/hive_boxes.dart';
-import 'package:weather_app/domain/hive/favourite_history/favourite_history.g.dart';
 import 'package:weather_app/weather_app.dart';
 
 Future<void> main() async {
